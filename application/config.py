4a092713262310f076545eaf4d1c1e5e706dbb45
Adhithya Laxman <94938999+Adhithya-Laxman@users.noreply.github.com>
2023-06-21 22:45:15 +0530

    Delete application directory

--- application/config.py
+++
@@ -1,7 +0,0 @@
-class Config():
-    DEBUG = False
-    SQLALCHEMY_DATABASE_URI = None
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-class LocalDevelopmentConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "postgresql://postgres:Password123@localhost:5432/bloodbank"
-    DEBUG = True
